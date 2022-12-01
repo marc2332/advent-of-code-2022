18b773399bef29619da223ca1e82c71e966a56b1
marc2332 <mespinsanz@gmail.com>
2022-12-01 19:49:57 +0100

    improved the code

--- day-rs/src/main.rs
+++ day-rs/src/main.rs
@@ -1,9 +1,9 @@
 static CONTENT: &str = include_str!("input");
 fn main() {
     let input = CONTENT.split("\n\n");
-    let elfs_calories = input.map(|e| e.split("\n").map(|n| n.parse().unwrap_or(0)));
-    let mut simplified = elfs_calories.map(|cals| cals.sum()).collect::<Vec<i32>>();
+    let elfs_calories = input.map(|e| e.split("\n").map(|n| n.parse::<i32>().unwrap()));
+    let mut simplified: Vec<i32> = elfs_calories.map(|cals| cals.sum()).collect();
     simplified.sort();
     simplified.reverse();
-    println!("first: {:?} second: {:?}", simplified[0], simplified[0] + simplified[1] + simplified[2]);
+    println!("first: {:?} second: {:?}", simplified[0], simplified.iter().take(3).sum::<i32>());
 }
