273b0fcf95ab95fbec2bc10ad69f2e5f093c4b83
marc2332 <mespinsanz@gmail.com>
2022-12-01 19:24:41 +0100

    first day but rusty

---
+++ day-rs/src/main.rs
@@ -0,0 +1,9 @@
+static CONTENT: &str = include_str!("input");
+fn main() {
+    let input = CONTENT.split("\n\n");
+    let elfs_calories = input.map(|e| e.split("\n").map(|n| n.parse().unwrap_or(0)));
+    let mut simplified = elfs_calories.map(|cals| cals.reduce(|a, b| a + b).unwrap()).collect::<Vec<i32>>();
+    simplified.sort();
+    simplified.reverse();
+    println!("first: {:?} second: {:?}", simplified[0], simplified[0] + simplified[1] + simplified[2]);
+}
