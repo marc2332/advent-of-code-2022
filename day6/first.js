b1956117b30304dc1ead2a17d5937affdd119117
marc2332 <mespinsanz@gmail.com>
2022-12-06 09:36:29 +0100

    day 6

---
+++ day6/first.js
@@ -0,0 +1,16 @@
+const content = await Deno.readTextFile("input", "UTF-8")
+let chars = [];
+let ind = 0;
+for(let i = 0; i < content.length; i++){
+    const c = content[i];
+    if(chars.includes(c)) {
+        let pos = chars.indexOf(c);
+        chars = [...chars.slice(pos + 1)]
+    } 
+    chars.push(c);
+    if(chars.length === 4){
+        ind = i + 1;
+        break;
+    }
+}
+console.log(chars, ind)
